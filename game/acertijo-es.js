72f535c839138fab59388524256bcdf966b0858c
dylux-fg <94474426+dylux-fg@users.noreply.github.com>
2021-12-05 06:23:10 -0400

    Add files via upload

---
+++ game/acertijo-es.js
@@ -0,0 +1,12 @@
+[ 
+{ "author" : "FG98",
+    "index": 1,
+    "soal": "Animales como las lagartijas que pueden cambiar el color de su piel.?",
+    "jawaban": "Camaleón"
+  },
+  { "author" : "FG98",
+    "index": 2,
+    "soal": "Carta de instrucción a Suharto para hacerse cargo de la seguridad de Indonesia el 11 de marzo de 1966?",
+    "jawaban": "Supersemar"
+  }
+] 
